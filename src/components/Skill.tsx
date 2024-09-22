87c383a9ddae1f8c85cacffccf6017a7ba811f26
minh.vo <dev.minhvo@gmail.com>
2024-09-22 16:18:07 +0700

    style(ui): svg for skills screen

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -12,16 +12,16 @@ interface Skill {
 
 const skills: Skill[] = [
   { name: 'Go', icon: '/icons/go.svg', level: 2 },
+  { name: 'Angular', icon: '/icons/angular.svg', level: 2 },
   { name: 'React', icon: '/icons/react.svg', level: 5 },
   { name: 'TypeScript', icon: '/icons/typescript.svg', level: 5 },
   { name: 'Next.js', icon: '/icons/nextjs.svg', level: 4 },
   { name: 'GraphQL', icon: '/icons/graphql.svg', level: 3 },
-  { name: 'Spring', icon: '/icons/spring.svg', level: 4 },
-  { name: 'Java', icon: '/icons/java.svg', level: 4 },
+  { name: 'Javascript', icon: '/icons/javascript.svg', level: 5 },
   { name: 'Solidity', icon: '/icons/solidity.svg', level: 3 },
   { name: 'MongoDB', icon: '/icons/mongodb.svg', level: 4 },
   { name: 'Node.js', icon: '/icons/nodejs.svg', level: 5 },
-  { name: 'Tailwind', icon: '/icons/tailwind.svg', level: 1 },
+  { name: 'Tailwind', icon: '/icons/tailwindcss.svg', level: 1 },
   { name: 'Docker', icon: '/icons/docker.svg', level: 3 },
 ];
 
@@ -141,13 +141,14 @@ const Skill = () => {
 
   return (
     <motion.div
+      id="experience"
       ref={containerRef}
       initial="hidden"
       animate={containerControls}
       variants={containerVariants}
-      className="bg-primary-dark p-4 sm:p-8 rounded-lg shadow-lg"
+      className="skills"
     >
-      <h2 className="text-2xl sm:text-3xl font-bold mb-4 sm:mb-6 text-center text-primary-light">My Skills</h2>
+      <h2 className="numbered-heading">Experience</h2>
       <motion.div
         ref={skillsRef}
         variants={skillsVariants}
@@ -159,33 +160,33 @@ const Skill = () => {
           <motion.div key={index} variants={itemVariants} className="bg-secondary-dark p-4 rounded-lg">
             <div className="flex items-center mb-2">
               <Image src={skill.icon} alt={skill.name} width={24} height={24} className="mr-2" />
-              <h3 className="text-lg font-semibold text-primary-light">{skill.name}</h3>
+              <h3 className="text-lg font-bold text-primary-light">{skill.name}</h3>
             </div>
             <div className="relative pt-1">
               <div className="flex mb-2 items-center justify-between">
                 <div>
-                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-primary-light bg-primary-dark">
+                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200">
                     {getLevelText(skill.level)}
                   </span>
                 </div>
                 <div className="text-right">
-                  <span className="text-xs font-semibold inline-block text-primary-light">
+                  <span className="text-xs font-semibold inline-block text-blue-600">
                     {skill.level * 20}%
                   </span>
                 </div>
               </div>
-              <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-primary-dark">
+              <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
                 <motion.div
                   variants={barVariants}
                   style={{ width: `${skill.level * 20}%` }}
-                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-accent"
+                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"
                 ></motion.div>
               </div>
             </div>
           </motion.div>
         ))}
       </motion.div>
-      <h3 className="text-xl sm:text-2xl font-bold mt-8 mb-4 text-center text-primary-light">Other Technologies</h3>
+      <h3 className="text-xl sm:text-2xl font-bold mt-8 mb-4 text-center text-lightest-slate">Other Technologies</h3>
       <motion.div
         ref={techRef}
         variants={techContainerVariants}
