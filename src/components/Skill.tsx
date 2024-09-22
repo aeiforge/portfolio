a708faad971a33d4e96e63515c12de276d535b2d
minh.vo <dev.minhvo@gmail.com>
2024-09-22 16:32:06 +0700

    style(icon): add fill color for svg

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -15,10 +15,10 @@ const skills: Skill[] = [
   { name: 'Angular', icon: '/icons/angular.svg', level: 2 },
   { name: 'React', icon: '/icons/react.svg', level: 5 },
   { name: 'TypeScript', icon: '/icons/typescript.svg', level: 5 },
-  { name: 'Next.js', icon: '/icons/nextjs.svg', level: 4 },
+  { name: 'NextJS', icon: '/icons/nestjs.svg', level: 4 },
   { name: 'GraphQL', icon: '/icons/graphql.svg', level: 3 },
   { name: 'Javascript', icon: '/icons/javascript.svg', level: 5 },
-  { name: 'Solidity', icon: '/icons/solidity.svg', level: 3 },
+  { name: 'RabbitMQ', icon: '/icons/rabbitmq.svg', level: 3 },
   { name: 'MongoDB', icon: '/icons/mongodb.svg', level: 4 },
   { name: 'Node.js', icon: '/icons/nodejs.svg', level: 5 },
   { name: 'Tailwind', icon: '/icons/tailwindcss.svg', level: 1 },
@@ -157,29 +157,33 @@ const Skill = () => {
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
       >
         {skills.map((skill, index) => (
-          <motion.div key={index} variants={itemVariants} className="bg-secondary-dark p-4 rounded-lg">
+          <motion.div 
+            key={index} 
+            variants={itemVariants} 
+            className="bg-primary-dark border border-secondary-dark p-4 rounded-lg"
+          >
             <div className="flex items-center mb-2">
               <Image src={skill.icon} alt={skill.name} width={24} height={24} className="mr-2" />
-              <h3 className="text-lg font-bold text-primary-light">{skill.name}</h3>
+              <h3 className="text-lg font-bold text-lightest-slate">{skill.name}</h3>
             </div>
             <div className="relative pt-1">
               <div className="flex mb-2 items-center justify-between">
                 <div>
-                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200">
+                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-secondary-dark bg-primary-dark border border-secondary-dark">
                     {getLevelText(skill.level)}
                   </span>
                 </div>
                 <div className="text-right">
-                  <span className="text-xs font-semibold inline-block text-blue-600">
+                  <span className="text-xs font-semibold inline-block text-secondary-dark">
                     {skill.level * 20}%
                   </span>
                 </div>
               </div>
-              <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
+              <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-primary-dark border border-secondary-dark">
                 <motion.div
                   variants={barVariants}
                   style={{ width: `${skill.level * 20}%` }}
-                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"
+                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-secondary-dark"
                 ></motion.div>
               </div>
             </div>
