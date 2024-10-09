472457e4311ee3044154066800470895d095a2f9
minh.vo <dev.minhvo@gmail.com>
2024-10-09 16:24:51 +0700

    feat: update thong profile

--- src/app/member/[name]/page.tsx
+++ src/app/member/[name]/page.tsx
@@ -283,6 +283,148 @@ const members: Record<string, Member> = {
       },
     ]
   },
+  thong: {
+    fullName: 'Pham Duc Thong',
+    role: 'Software Engineer',
+    slogan: 'Software Engineer',
+    description: [
+      "Hi, I'm  {highlight}Thong{/highlight}. I have nearly 3 years of experience as a {highlight}Software Engineer{/highlight} with skills across oil & gas. {link}Learn more!{/link}",
+    ],
+    shortIntro:
+      "I'm a fullstack developer with a passion for building web/mobile applications.",
+    image: '/images/vinhdang.jpg',
+    contact: {
+      phone: '+84 948 643 435',
+      email: {
+        name: 'phamducthong11101998@gmail.com',
+        url: 'mailto:phamducthong11101998@gmail.com',
+        icon: '/icons/email.svg',
+        hide: false,
+      },
+      instagram: {
+        name: 'instagram.com/mmiinn.06',
+        url: 'https://www.instagram.com/thong_pd/',
+        icon: '/icons/instagram.svg',
+        hide: false,
+      },
+      skype: {
+        name: 'live:.cid.a2a8e4905c051bfa',
+        url: 'https://join.skype.com/invite/vScj21vZxsI1',
+        icon: '/icons/skype.svg',
+        hide: false,
+      },
+      linkedin: {
+        name: 'https://www.linkedin.com/in/thong1/',
+        url: 'https://www.linkedin.com/in/thong1/',
+        icon: '/icons/linkedin.svg',
+        hide: false,
+      },
+      github: {
+        name: 'github.com/aeiforge',
+        url: 'https://github.com/aeiforge',
+        icon: '/icons/github.svg',
+        hide: false,
+      },
+      twitter: {
+        name: 'twitter.com/vinhdk',
+        url: 'https://twitter.com/vinhdk',
+        icon: '/icons/twitter.svg',
+        hide: false,
+      },
+      codepen: {
+        name: 'codepen.io/vinhdk',
+        url: 'https://codepen.io/vinhdk',
+        icon: '/icons/codepen.svg',
+        hide: true,
+      },
+      location: 'Ho Chi Minh City, Vietnam',
+      status: 'Available for new projects',
+    },
+    techstack: {
+      mainStackList: [
+        { name: 'HTML5', icon: '/icons/html5.svg', frequency: 5 },
+        { name: 'Sass', icon: '/icons/sass.svg', frequency: 5 },
+        { name: 'Tailwind', icon: '/icons/tailwindcss.svg', frequency: 4 },
+        { name: 'TypeScript', icon: '/icons/typescript.svg', frequency: 5 },
+        { name: 'JavaScript', icon: '/icons/javascript.svg', frequency: 4 },
+        { name: 'Angular', icon: '/icons/angular.svg', frequency: 5 },
+        { name: 'Ngrx', icon: '/icons/ngrx.svg', frequency: 5 },
+        { name: 'Rxjs', icon: '/icons/reactivex.svg', frequency: 5 },
+        { name: 'React', icon: '/icons/react.svg', frequency: 3 },
+        { name: 'NextJS', icon: '/icons/nestjs.svg', frequency: 2 },
+        { name: 'ThreeJS', icon: '/icons/threedotjs.svg', frequency: 2 },
+        { name: 'NodeJS', icon: '/icons/nodejs.svg', frequency: 4 },
+        { name: 'GraphQL', icon: '/icons/graphql.svg', frequency: 3 },
+        { name: 'MongoDB', icon: '/icons/mongodb.svg', frequency: 3 },
+        { name: 'PostgreSQL', icon: '/icons/postgresql.svg', frequency: 5 },
+      ],
+      otherStackList: [
+        'Docker',
+        'Kubernetes',
+        'AWS',
+        'Kafka',
+        'RabbitMQ',
+        'Go',
+        'Zod',
+        'Redux',
+        'PostgreSQL',
+        'Vercel',
+        'Netlify',
+        'Material UI',
+        'PrimeNG',
+        'Ant Design',
+        'Webpack',
+        'Remix',
+        'Vite',
+        'Babel',
+        'Jest',
+        'Storybook',
+        'Nx',
+      ],
+      keyTechnologies: [
+        { name: 'JavaScript', icon: '/icons/javascript.svg' },
+        { name: 'TypeScript', icon: '/icons/typescript.svg' },
+        { name: 'Angular', icon: '/icons/angular.svg' },
+        { name: 'Nest.js', icon: '/icons/nestjs.svg' },
+        { name: 'Ngrx', icon: '/icons/ngrx.svg' },
+        { name: 'Rxjs', icon: '/icons/reactivex.svg' },
+        { name: 'HTML5', icon: '/icons/html5.svg' },
+        { name: 'Sass', icon: '/icons/sass.svg' },
+        { name: 'Tailwind', icon: '/icons/tailwindcss.svg' },
+      ],
+      keyTechnologiesDescription:
+        "These are the key tech stacks I've {highlight}frequently used{/highlight} during project development. {link}Learn more!{/link}",
+      futureTechnologies: [
+        { name: 'Go', icon: '/icons/go.svg' },
+        { name: 'Gin', icon: '/icons/gin.svg' },
+        { name: 'Deno', icon: '/icons/deno.svg' },
+        { name: 'Remix', icon: '/icons/remix.svg' },
+        { name: 'Three.js', icon: '/icons/threedotjs.svg' },
+        { name: 'Lambda', icon: '/icons/awslambda.svg' },
+        { name: 'Lerna', icon: '/icons/lerna.svg' },
+      ],
+      futureTechnologiesDescription:
+        "These are the key tech stacks I'm {highlight}currently learning{/highlight} and {highlight}interested in{/highlight} the next few months.",
+    },
+    modelUrl: '/models/fixed/thong.glb',
+    expectations: [
+      'I am currently here looking for a {highlight}second job as a freelancer{/highlight}. Although based in Ho Chi Minh City, Vietnam, I am very flexible with {highlight}time zone communications and locations{/highlight}. Please feel free to {link}contact me{/link} if you are looking for someone who can meet your job requirements.',
+    ],
+    jobs: [
+      {
+        companyLogo: '/images/halliburton.png',
+        companyName: 'Halliburton',
+        position: 'Senior Software Engineering',
+        duration: '09/2023 - Present',
+        description: [
+          'A major Oil and Gas system used across some contries such as Italy, Saudi Arabia, Thailand, Malaysia, Brunei and India, designed specifically for engineers to {highlight}perform critical calculations and generate comprehensive reports{/highlight}',
+        ],
+        link: 'https://www.halliburton.com/',
+        logoWidth: 150,
+        logoHeight: 90,
+      }
+    ]
+  },
 };
 
 
